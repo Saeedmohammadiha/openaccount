087b8b9c8df2134c79c9d831525b5c557cbfbc7e
saeed mohammdiha <saeed1440124@gmail.com>
2023-11-29 15:22:19 +0330

    wip working on form

--- src/pages/BasicInfo.js
+++ src/pages/BasicInfo.js
@@ -1,3 +1,37 @@
+import { Grid, TextField } from "@material-ui/core";
+import { Form, useForm, Controller  } from "react-hook-form";
+
 export default function BasicInfo() {
-  return <div>basic info</div>;
+
+
+     
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container direction="column" spacing={4}>
+        <Grid item>
+          <TextField
+          name="username"
+          //  inop={register}
+            id="account-type"
+            label="نوع حساب"
+            variant="outlined"
+            // error={"test"}
+            // helperText="Incorrect entry."
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+         
+            id="mobile-number"
+            label="شماره موبایل"
+            variant="outlined"
+            // error={"test"}
+            // helperText="Incorrect entry."
+          />
+        </Grid>
+      </Grid>
+    </form>
+  );
 }
